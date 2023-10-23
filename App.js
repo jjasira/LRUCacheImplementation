905a840d0a1d91b6c0dd764c65a010a0dd74b0e3
jjasira <107365520+jjasira@users.noreply.github.com>
2023-10-23 12:48:47 +0300

    LRUCache in React
    LRU cache implemented in react  using a linked list. Custom Hook applicaion

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DynamicContentLoader from "./DynamicContentLoader";
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicContentLoader />
+    </div>
+  );
+}
+
+export default App;
---
+++ useLRUCache.js
@@ -0,0 +1,98 @@
+import { useRef } from "react";
+
+class LRUCache {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.cache = {};
+    this.head = null;
+    this.tail = null;
+  }
+  get(key) {
+    if (this.cache[key]) {
+      this.moveToFront(key);
+      return this.cache[key].value;
+    }
+
+    return null;
+  }
+
+  put(key, value) {
+    if (this.cache[key]) {
+      this.cache[key].value = value;
+      this.moveToFront(key);
+    } else {
+      if (Object.keys(this.cache).length === this.capacity) {
+        this.removeLast();
+      }
+
+      this.addToFront(key, value);
+    }
+  }
+  addToFront(key, value) {
+    const newNode = {
+      key,
+      value,
+      next: null,
+    };
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.cache[key] = newNode;
+  }
+  moveToFront(key) {
+    const current = this.cache[key];
+    if (current === this.head) return;
+
+    let prev = null;
+    let node = this.head;
+
+    while (node && node.key !== key) {
+      prev = node;
+      node = node.next;
+    }
+
+    if (!node) return;
+
+    if (node === this.tail) {
+      this.tail = prev;
+    }
+    if (prev) {
+      prev.next = node.next;
+    }
+    node.next = this.head;
+    this.head = node;
+  }
+  removeLast() {
+    if (!this.head) return;
+    const lastkey = this.tail.key;
+    delete this.cache[lastkey];
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let current = this.head;
+      while (current.next !== this.tail) {
+        current = current.next;
+      }
+      current.next = null;
+      this.tail = current;
+    }
+  }
+}
+
+const useLRUCache = (capacity) => {
+  const cacheRef = useRef(new LRUCache(capacity));
+  console.log(cacheRef.current);
+  return {
+    get: (key) => cacheRef.current.get(key),
+    put: (key, value) => cacheRef.current.put(key, value),
+  };
+};
+
+export default useLRUCache;
